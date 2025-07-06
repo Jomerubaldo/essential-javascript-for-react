8ad43f0af8bc2fd9df3d9ca6fca6b9272e865f60
Your Name <clear>
2025-07-07 02:54:27 +0800

    initial commit

---
+++ destructuring.js
@@ -0,0 +1,17 @@
+//Duestructuring Array
+const array = ['Jomer', 21, 'BSIT'];
+
+const [name1, age1, course1] = array;
+
+console.log(name1, age1, course1);
+
+//Destructuring object
+const object = {
+  name2: 'Jomer',
+  age2: 21,
+  course2: 'BSIT',
+};
+
+const { name2, age2, course2 } = object;
+
+console.log(name2, age2, course2);
---
+++ ternaryOperator.js
@@ -0,0 +1,6 @@
+//Ternary Operator
+let grade = 75;
+let condition =
+  grade >= 90 ? 'Excellent' : grade >= 75 ? 'Passed' : 'Try again';
+
+console.log('OverAll Remark:', condition);
